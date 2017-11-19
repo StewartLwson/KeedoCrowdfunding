29a9c8a612848fb4721a95d393bb57b407bb652f
StewartLwson <stewartlwson@gmail.com>
2017-11-19 17:55:00 +0000

    created react grid component

--- components/App.js
+++ components/App.js
@@ -4,12 +4,15 @@ import React from 'react';
 // Importing app specific components
 import { AppNavbar } from './Navbar';
 import { AppCarousel } from './Carousel';
+import { ProjectGrid } from './ProjectGrid';
 
 export const App = () => {
   return (
     <div>
       <AppNavbar />
       <AppCarousel />
+      <br />
+      <ProjectGrid />
     </div>
   );
 }
---
+++ components/ProjectGrid.js
@@ -0,0 +1,19 @@
+// Importing React
+import React from 'react';
+
+// Importing ReactBootstrap grid components
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+
+export const ProjectGrid = () => {
+  return (
+    <Grid>
+      <Row className='show-grid'>
+        <Col xs={6} md={4}></Col>
+        <Col xs={6} md={4}></Col>
+        <Col xs={6} md={4}></Col>
+      </Row>
+    </Grid>
+  );
+}
