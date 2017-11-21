97fd53e444a69771f8427aa38f7d06150c8e7f83
StewartLwson <stewartlwson@gmail.com>
2017-11-21 16:44:33 +0000

    created sign in (unfinished)

---
+++ components/SignIn.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Button from 'react-bootstrap/lib/Button';
+import Glyphicon from 'react-bootstrap/lib/Glyphicon';
+import Form from 'react-bootstrap/lib/Form';
+import FormGroup from 'react-bootstrap/lib/FormGroup';
+import FormControl from 'react-bootstrap/lib/FormControl';
+
+import Popover from 'react-bootstrap/lib/Popover';
+
+export const SignIn = () =>{
+  return (
+    <Popover
+      id="popover-basic"
+      placement="top"
+      positionLeft={200}
+      positionTop={50}
+      title="Sign In"
+    >
+  )
+}
