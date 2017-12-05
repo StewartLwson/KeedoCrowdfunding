8a8b330da65ea26270f82b5ed2bae5ab51637ccd
Stewart Lawson <stewartlwson@gmail.com>
2017-12-05 15:16:54 +0000

    database implemeneted but not working

--- components/Projects.js
+++ components/Projects.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+// Initializing firebase
+import fire from '../fire';
+
 // Importing page formatting components
 import Grid from 'react-bootstrap/lib/Grid';
 import Row from 'react-bootstrap/lib/Row';
@@ -8,18 +11,38 @@ import Col from 'react-bootstrap/lib/Col';
 import { Project } from './Project';
 import { SearchBar } from './SearchBar';
 
-var ref = firebase.database().ref().child('heading')
-console.log(ref);
-
-function test() {
-  ref.on('value', function(snapshot) {
-    console.log(snapshot.val());
-  });
-}
-
-test;
-
 export class Projects extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ids: [],
+      names: []
+    };
+  }
+
+  componentWillMount() {
+    for(let i = 0; i <= 999; i++) {
+      let no = i.toString();
+      if(no.length == 1) {
+        no = "00" + no;
+      } else if(no.length == 2) {
+        no = "0" + no;
+      }
+      let projectRef = fire.database().ref('project_names/' + no);
+      projectRef.on('value', (snapshot) => {
+        if (snapshot.val() !== null) {
+          this.setState( {
+            ids: this.state.ids.push(snapshot.key),
+            names: this.state.names.push(snapshot.val())
+          });
+
+        }
+      });
+    }
+    console.log(this.state.ids);
+    console.log(this.state.names);
+  };
+
   render() {
     return (
       <div>
@@ -29,7 +52,7 @@ export class Projects extends React.Component {
         <Grid>
           <Row className='show-grid'>
             <Col xs={6} md={12}>
-              <h1>Projects</h1>
+              <h1>Project</h1>
               <hr />
             </Col>
           </Row>
@@ -57,4 +80,4 @@ export class Projects extends React.Component {
       </div>
     );
   }
-}
+};
